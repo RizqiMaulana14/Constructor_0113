21563109bf617f9c5bc3b02b1a3b3c0caa031804
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-13 18:56:59 +0700

    Integer Main

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -14,3 +14,9 @@ mahasiswa::mahasiswa(int nim, string nama){
     cout << "NIM   : " << nim << endl;
     cout << "Nama  :" << nama << endl;
 }
+
+int main(){
+    //Memanggil Constructor Dengan Parameter
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
+}
