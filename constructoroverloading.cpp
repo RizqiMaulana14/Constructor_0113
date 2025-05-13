ab62585bf55d1e917f2277cdc90167abc11b709f
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-13 19:05:52 +0700

    Definisikan

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -13,3 +13,14 @@ class mahasiswa{
         mahasiswa(int iNim, string iNama);
         void cetak();
 };
+
+mahasiswa::mahasiswa(){
+}
+
+mahasiswa::mahasiswa(int iNim){
+    nim = iNim; //Definisi Hanya NIM
+}
+
+mahasiswa::mahasiswa(string iNama){
+    nama = iNama; //Definisi Hanya Nama
+}
