74833c87f0f25b84bec36973f8cf90a3dcfa2b75
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-13 19:28:29 +0700

    Integer Main

--- chainfunctiioncall.cpp
+++ chainfunctiioncall.cpp
@@ -12,3 +12,10 @@ class buku{
         return this -> judul;
     }
 };
+
+int main(){
+    buku bukunya;
+    //chain function calls
+    cout << bukunya.setJudul("Matematika").getJudul();
+    return 0;
+}
