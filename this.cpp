334749834916d80f759415b0da1dad3dfdce9a12
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-05-13 19:20:54 +0700

    Pembuatan This

---
+++ this.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class buku{
+    string judul;
+    public:
+        string setget(string jdl){
+            //Nilai parameter 'judul' untuk member cariabel 'judul'
+            this -> judul = jdl;
+            //return variabel judul.
+            return this -> judul;
+        }
+};
+
